93e5a68dd8be686bac38129cd759df846e1ff769
Tiago Fujii <tiagoyukio12@gmail.com>
2022-07-03 19:22:07 -0300

    Use Jamdict to look up definitions
    Uses jamdict library (https://github.com/neocl/jamdict) instead of
    converting mobi dictionary to find word definitions.

--- convert_vocab.py
+++ convert_vocab.py
@@ -8,13 +8,12 @@ from datetime import datetime
 from operator import itemgetter
 from operator import attrgetter
 from collections import namedtuple
-
+from jamdict import Jamdict
 
 AnkiNote = namedtuple('AnkiNote', 'word usage definition timestamp')
 
 
 def get_vocab(vocab_db, _since=0):
-
     if isinstance(_since, datetime):
         since = int(_since.timestamp()) * 1000
     else:
@@ -30,23 +29,24 @@ def get_vocab(vocab_db, _since=0):
         left join BOOK_INFO
         on BOOK_INFO.id = LOOKUPS.book_key
         where LOOKUPS.timestamp > ?
+        and BOOK_INFO.title = '大きな帽子の女'
         order by WORDS.stem, LOOKUPS.timestamp
     '''
     rows = cur.execute(sql, (since,)).fetchall()
     return rows
 
 
-def make_notes(vocab, dict_tsv, include_nodef=False):
-
+def make_notes(vocab, include_nodef=False):
     # OALD is ~38 MiB for each stem and ~110 MiB for each iform,
     # read it all into a Python dict does not hurt on mordern PCs.
-    with open(dict_tsv, 'r', encoding='utf-8') as f:
-        dict_db = dict(line.split('\t') for line in f.readlines())
+    jam = Jamdict()
 
     stems_no_def = set()
     notes = []
     # vocab is a list of db rows order by (stem, timestamp)
     for stem, entries in itertools.groupby(vocab, itemgetter('stem')):
+        if stem is None:
+            continue
         # Merge multiple usages into one.
         usage_all = ''
         usage_timestamp = None
@@ -59,7 +59,15 @@ def make_notes(vocab, dict_tsv, include_nodef=False):
 
         # Look up definition in dictionary
         try:
-            definition = dict_db[stem].strip()
+            result = jam.lookup(stem)
+            definition = ''
+            for entry in result.entries:
+                definition += f'<b>{str(entry.kana_forms[0])}'
+                if len(entry.kanji_forms) > 0 and len(result.entries) > 1:
+                    definition += f' ({str(entry.kanji_forms[0])})'
+                definition += '</b>:<br>'
+                for i, sense in enumerate(entry.senses):
+                    definition += f'{i + 1}. {str(sense)}<br>'
         except KeyError:
             stems_no_def.add(stem)
             if include_nodef:
@@ -77,7 +85,6 @@ def make_notes(vocab, dict_tsv, include_nodef=False):
 
 
 def output_anki_tsv(notes, output, sort=True):
-
     if sort:
         notes.sort(key=attrgetter('timestamp'), reverse=True)
 
@@ -89,12 +96,13 @@ def output_anki_tsv(notes, output, sort=True):
 
 if __name__ == '__main__':
     argp = argparse.ArgumentParser()
-    argp.add_argument('--since', type=lambda s: datetime.strptime(s, '%Y-%m-%d'), default=datetime.utcfromtimestamp(86400 * 2))  # Windows workaround
-    argp.add_argument('--include-nodef', action='store_true', help='include words that have no definitions in dictionary')
+    argp.add_argument('--since', type=lambda s: datetime.strptime(s, '%Y-%m-%d'),
+                      default=datetime.utcfromtimestamp(86400 * 2))  # Windows workaround
+    argp.add_argument('--include-nodef', action='store_true',
+                      help='include words that have no definitions in dictionary')
     argp.add_argument('vocab_db')
-    argp.add_argument('dict_tsv')
     argp.add_argument('anki_tsv', type=argparse.FileType('w', encoding='utf-8'))
     args = argp.parse_args()
     vocab = get_vocab(args.vocab_db, _since=args.since)
-    notes = make_notes(vocab, args.dict_tsv, include_nodef=args.include_nodef)
+    notes = make_notes(vocab, include_nodef=args.include_nodef)
     output_anki_tsv(notes, args.anki_tsv)
